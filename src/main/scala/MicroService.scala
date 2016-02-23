56fb9a46fdab03a475e3285e34f2694b8d4422c6
Venu Gangireddy <venu.gangireddy@gmail.com>
2016-02-23 19:05:33 +0530

    compiled, testing

--- src/main/scala/MicroService.scala
+++ src/main/scala/MicroService.scala
@@ -2,11 +2,13 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
 import akka.http.scaladsl.marshalling.Marshal
-import akka.stream.{ActorMaterializer}
+import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshalling.PredefinedToResponseMarshallers
 import akka.http.scaladsl.model._
+
+
 
 object Api {
   val route:Route = get {
@@ -14,7 +16,7 @@ object Api {
       complete { HttpResponse(200, entity = "cool, you have two options /ping or /crash") }
     } ~
     path("ping") {
-      complete { "ping" }
+      complete { "pong" }
     } ~
     path("crash"){
       complete { "boom" }
@@ -22,20 +24,23 @@ object Api {
   }
 }
 
-object CustomeRejectionHandler extends PredefinedToResponseMarshallers{
-  import StatusCodes._
+object CustomeRejectionHandler {
+  import scala.reflect.ClassTag
 
-  private def annonomous(route:Route): Route = complete (HttpResponse(401, entity = "BadBadBad"))
+  private def annonomous(route:Route): Route = complete ( HttpResponse(401, entity = "401 is BaaD M'kay"))
 
-  implicit def jsonRejectionHandler: RejectionHandler = {
-    rejctionSeq: scala.collection.immutable.Seq[Rejection] => RejectionHandler.default(rejctionSeq).map(annonomous)
-  }
+  implicit def jsonRejectionHandler[T <: Rejection: ClassTag]: RejectionHandler = RejectionHandler.newBuilder().handleAll[T] { rejections =>
+    RejectionHandler.default(rejections).map(annonomous).getOrElse(complete { HttpResponse(401, entity = "401 is BaaD M'kay") } )
+  }.result()
+
 }
+
+
+import CustomeRejectionHandler.jsonRejectionHandler
 
 object MicroService extends App {
 
-  import Api._
-  import CustomeRejectionHandler._
+  import Api.route
 
   implicit val system = ActorSystem()
   implicit val exec = system.dispatcher
