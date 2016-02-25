fa96932e5768b7faebe63eaf32ee6c6434655257
Venu Gangireddy <venu.gangireddy@gmail.com>
2016-02-25 12:16:04 +0530

    first working model

--- src/main/scala/MicroService.scala
+++ src/main/scala/MicroService.scala
@@ -9,7 +9,6 @@ import akka.http.scaladsl.marshalling.PredefinedToResponseMarshallers
 import akka.http.scaladsl.model._
 
 
-
 object Api {
   val route:Route = get {
     pathSingleSlash {
@@ -26,12 +25,28 @@ object Api {
 
 object CustomeRejectionHandler {
   import scala.reflect.ClassTag
+  import akka.http.scaladsl.model.MediaTypes.`application/json`
+  import scala.concurrent.ExecutionContext.Implicits.global
+  //private def annonomous(route:Route): Route = complete (HttpResponse(404, entity = "401 is BaaD M'kay"))
 
-  private def annonomous(route:Route): Route = complete (HttpResponse(401, entity = "401 is BaaD M'kay"))
+  implicit def jsonRejectionHandler: RejectionHandler =
+    RejectionHandler.newBuilder()
+    .handleAll[Rejection] { rejections =>
+      ctx: RequestContext => {
 
-  implicit def jsonRejectionHandler[T <: Rejection: ClassTag]: RejectionHandler = RejectionHandler.newBuilder().handleAll[T] { rejections =>
-    RejectionHandler.default(rejections).map(annonomous).getOrElse(complete { HttpResponse(401, entity = "401 is BaaD M'kay") } )
-  }.result()
+          val route: Route = RejectionHandler.default(rejections).getOrElse( complete { HttpResponse(404, entity = HttpEntity(`application/json`, """{ "message": "404 is BaaD M'kay" }""")) } )
+
+          route.apply(ctx).map { res : RouteResult => res match {
+              case resp@RouteResult.Complete(HttpResponse(a, b, c, d)) => RouteResult.Complete(HttpResponse(a, b, HttpEntity(`application/json`, """{ "message": "404 is BaaD M'kay" }"""),d))
+              /*ignoring further seq of rejection here, check this*/
+              case resp@RouteResult.Rejected(seqOfRejections) => {
+                RouteResult.Complete( HttpResponse(404, entity = HttpEntity(`application/json`, """{ "message": "404 is BaaD M'kay" }""")))
+              }
+            }
+          }
+        }
+    }
+    .result()
 
 }
 
